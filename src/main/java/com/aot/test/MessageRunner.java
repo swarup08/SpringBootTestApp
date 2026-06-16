bfe95cc4555878049b0c888cc403e445e24eccef
Swarup <swarup26.sarkar@gmail.com>
2026-06-16 13:12:02 +0530

    WH-998 Next Commit

--- src/main/java/com/aot/test/MessageRunner.java
+++ src/main/java/com/aot/test/MessageRunner.java
@@ -9,7 +9,7 @@ public class MessageRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Welcome to Spring Boot App!");
-
+		
 	}
 
 }
